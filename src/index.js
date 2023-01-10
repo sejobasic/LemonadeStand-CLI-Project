ce871ff0678eceac04a7c74fc2f9ace47bde2d3f
Sejo Basic <sejoob@gmail.com>
2023-01-10 12:16:28 -0700

    Imported prompt function from Vorpal to save user response in objects

--- src/index.js
+++ src/index.js
@@ -53,4 +53,61 @@ vorpal
     callback()
   })
 
+vorpal
+  .command(
+    'Order <name> <phoneNumber>',
+    'Create an order and saves it as a JSOn file'
+  )
+  .action(function (args, callback) {
+    const order = {
+      customer: {
+        name: args.name,
+        phoneNumber: args.phoneNumber,
+      },
+      lemonadeStand: {
+        name: 'Sejos Lemonade Stand',
+      },
+    }
+
+    // Prompt the user for how many lemonades they want
+    this.prompt(
+      {
+        type: 'number',
+        name: 'numLemonades',
+        message: 'How many lemonades would you like to order?',
+      },
+      (userResp) => {
+        const userNum = Number.parseInt(userResp.numLemonades)
+        let lemonade = {}
+
+        const questions = []
+        for (let i = 1; i <= userNum; i++) {
+          questions.push({
+            type: 'number',
+            name: 'lemonJuice' + i,
+            message: `How many cups of lemon juice do you want in your lemonade ${i}?"`,
+          })
+          questions.push({
+            type: 'number',
+            name: 'water' + i,
+            message: `How many cups of water do you want in your lemonade ${i}?"`,
+          })
+          questions.push({
+            type: 'number',
+            name: 'sugar' + i,
+            message: `How many cups of sugar do you want in your lemonade ${i}?"`,
+          })
+          questions.push({
+            type: 'number',
+            name: 'iceCubes' + i,
+            message: `How many ice cubes do you want in your lemonade ${i}?"`,
+          })
+          this.prompt(questions, (userResp) => {
+            this.log(userResp)
+            callback()
+          })
+        }
+      }
+    )
+  })
 vorpal.show()

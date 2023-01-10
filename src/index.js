430103838d590b8ec670744b3af01502505478a2
Sejo Basic <sejoob@gmail.com>
2023-01-10 11:12:57 -0700

    Order Data Initialized

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-const lemonade = {
-  lemonJuice: 3,
-  water: 2,
-  sugar: 1.5,
-  iceCubes: 3,
-  getLemonJuice() {
-    return this.lemonJuice
+const order = {
+  total: 5.0,
+  lemonades: [
+    {
+      lemonJuice: 4,
+      water: 2,
+      sugar: 3,
+      iceCubes: 7,
+    },
+    {
+      lemonJuice: 4,
+      water: 3,
+      sugar: 2,
+      iceCubes: 3,
+    },
+    {
+      lemonJuice: 2,
+      water: 2,
+      sugar: 3,
+      iceCubes: 2,
+    },
+    {
+      lemonJuice: 2,
+      water: 2,
+      sugar: 1,
+      iceCubes: 4,
+    },
+  ],
+  lemonadeStand: [
+    {
+      name: 'Sejos Lemonade Stand',
+    },
+  ],
+  customer: {
+    name: 'Jack',
+    phoneNumber: '222-222-2222',
   },
-  calculatePrice() {
-    return (
-      this.lemonJuice * 0.3 +
-      this.water * 0.01 +
-      this.sugar * 0.25 +
-      this.iceCubes * 0.05 + 0.75
-    )
-  }
 }
-
-function updateLemonade(lemonade, lemonJuice, water, sugar, iceCubes) {
-  return {
-    ...lemonade,
-    lemonJuice,
-    water,
-    sugar,
-    iceCubes
-  }
-}
-console.log(updateLemonade(lemonade, 1, 2, 3, 4))
-
-const nums = [1, 2, 3, 4, 5]
-const numbersCopy = [...nums]
-numbersCopy[5] = 3
-console.log(numbersCopy)
-
-
-let {water, lemonJuice, sugar, iceCubes} = lemonade
-
-console.log(water, lemonJuice, sugar, iceCubes)
